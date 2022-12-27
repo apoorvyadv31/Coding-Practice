b7123354551304ae77208ff7e04450c514bd5ad9
Apoorv Yadav <75567911+apoorvyadv31@users.noreply.github.com>
2022-12-27 07:41:06 +0530

    Add files via upload

---
+++ hs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class hs {
+    public static void main(String[] args) {
+        HashSet <Integer>set=new HashSet<>();
+        set.add(5);
+        set.add(15);
+        set.add(10);
+        if (set.contains(10))
+        System.out.println("CONTAINS");
+     
+
+     System.out.println(set);
+    }
+}
