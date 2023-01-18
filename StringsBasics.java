3786a69ae6948d7b1a4d1de09ce037de48a6c4c5
Apoorv Yadav <75567911+apoorvyadv31@users.noreply.github.com>
2023-01-18 22:19:37 +0530

    Add files via upload

---
+++ StringsBasics.java
@@ -0,0 +1,12 @@
+public class StringsBasics {
+    public static void main(String[] args) {
+        String str ="Apoorv";
+        System.out.println(str.length());
+    System.out.println(str.charAt(4));
+    System.out.println(str.substring(3));
+    System.out.println(str.substring(1,4));
+    String strr ="Angel";
+    String strr1="gel";
+    System.out.println(strr.indexOf(strr1));
+    }
+}
